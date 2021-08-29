831385d74913953a1ce0b014d3a889fe061681b6
AndrewAK1619 <andrzej.kaminski0591@gmail.com>
2021-08-29 13:59:49 +0200

    Put to security userDetailsService and passwordEncoder

--- src/main/java/com/example/shop/config/SecurityConfig.java
+++ src/main/java/com/example/shop/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.example.shop.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         // stateless - czyści informacje
         // statefull - nie czyści, przechowuje informacje
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // włożyliśmy do security userDetailsService, passwordEncoder
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 }
